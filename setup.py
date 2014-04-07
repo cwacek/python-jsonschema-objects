f4e0a4f339a2869eee167b58132ce9652c816e4d
Chris Wacek <cwacek@gmail.com>
2014-04-07 17:59:27 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ if __name__ == '__main__':
         long_description = ''
 
     setup(name='python_jsonschema_objects',
-          version='0.0.1',
+          version='0.0.2',
           description='An object wrapper for JSON Schema definitions',
           author='Chris Wacek',
           long_description=long_description,
