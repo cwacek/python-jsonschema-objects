e17a8e3014b374277cd4d697bdd087cbbf827289
Matthew Broomfield <mbroomfield@rapid7.com>
2017-12-05 08:44:33 -0500

    Make a patch to better support null types.
    This commit attempts to permit missing parameters if their type could be
    the `null` type.
    
    Currently null types are not supported 100% in
    python_jsonschema_objects.
    
    Assuming a schema where the type of a variable `foo` is `null`, and
    where strict enforcing was used for schema validation:
    * `Object(foo=None)` should be valid as is as of this commit
    * `Object() should NOT be valid as is as of this commit`

--- python_jsonschema_objects/classbuilder.py
+++ python_jsonschema_objects/classbuilder.py
@@ -292,9 +292,23 @@ class ProtocolBase(collections.MutableMapping):
         """
 
         propname = lambda x: self.__prop_names__[x]
-        missing = [x for x in self.__required__
-                   if propname(x) not in self._properties or
-                   self._properties[propname(x)] is None]
+        missing = []
+        for x in self.__required__:
+
+            # Allow the null type
+            propinfo = self.propinfo(propname(x))
+            nulltype = False
+            if 'type' in propinfo:
+                nulltype = propinfo['type'] == 'null'
+            elif 'oneOf' in propinfo:
+                for o in propinfo['oneOf']:
+                    if 'type' in o and o['type'] == 'null':
+                        nulltype = True
+                        break
+
+            if (propname(x) not in self._properties and nulltype) or \
+                    (self._properties[propname(x)] is None and not nulltype):
+                missing.append(x)
 
         if len(missing) > 0:
             raise validators.ValidationError(
