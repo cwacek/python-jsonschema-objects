b95bd34c9e5ebcf8dc4ccb00225135d0a11e1771
Mark Atkinson <atkin901@gmail.com>
2018-01-25 08:29:32 -0800

    Add regression test for pull request 114, issue 111

---
+++ test/test_regression_114.py
@@ -0,0 +1,32 @@
+import pytest
+import python_jsonschema_objects as pjo
+
+
+def test_114():
+    schema = {
+        "title": "Example",
+        "type": "object",
+        "properties": {
+            "test_regression_114_anon_array": {
+                "type": "array",
+                "items": [
+                    {
+                        "oneOf": [
+                            {
+                                "type": "string",
+                                "pattern": "^(?:(?:\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5])\\.){3}(?:\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5])$"
+                            },
+                            {
+                                "type": "string",
+                                "pattern": "^((([0-9A-Fa-f]{1,4}:){1,6}:)|(([0-9A-Fa-f]{1,4}:){7}))([0-9A-Fa-f]{1,4})$"
+                            }
+                        ]
+                    }
+                ]
+            }
+        }
+    }
+
+    builder = pjo.ObjectBuilder(schema)
+    test = builder.build_classes()
+    assert test
