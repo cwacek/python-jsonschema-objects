57195ade3c4a7a493a76a7b892fcc90d1e2a84d4
Kenny Do <kedo@yelp.com>
2019-05-24 14:18:18 -0700

    Test in py37 as well

--- setup.py
+++ setup.py
@@ -88,6 +88,8 @@ if __name__ == '__main__':
               'Programming Language :: Python :: 2.7',
               'Programming Language :: Python :: 3',
               'Programming Language :: Python :: 3.5',
+              'Programming Language :: Python :: 3.6',
+              'Programming Language :: Python :: 3.7',
               'Intended Audience :: Developers',
               'Development Status :: 4 - Beta',
               'License :: OSI Approved :: MIT License',
