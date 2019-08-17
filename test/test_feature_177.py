04732673b920ce8f6ff6fb02b71dda5a05feb095
Chris Wacek <cwacek@gmail.com>
2019-08-17 14:29:37 -0400

    feature: Support pjo literals as attribute keys.
    This fixes #177

---
+++ test/test_feature_177.py
@@ -0,0 +1,38 @@
+import pytest
+
+import python_jsonschema_objects as pjo
+
+
+@pytest.fixture
+def classes():
+
+    schema = {
+        "title": "Config",
+        "type": "object",
+        "additionalProperties": {"$ref": "#/definitions/Parameter"},
+        "definitions": {
+            "Parameter": {
+                "$id": "Parameter",
+                "type": "object",
+                "properties": {
+                    "key": {"type": "string"},
+                    "value": {"oneOf": [{"type": "string"}, {"type": "number"}]},
+                },
+            }
+        },
+    }
+
+    return pjo.ObjectBuilder(schema).build_classes()
+
+
+def test_pjo_objects_can_be_used_as_property_keys(classes):
+    container = classes.Config()
+    obj1 = classes.Parameter(key="volume", value=11)
+    obj2 = classes.Parameter(key="compression", value="on")
+
+    container[obj1.key] = obj1
+    container[obj2.key] = obj2
+
+    assert container.volume.value == 11
+
+    assert sorted(container.keys()) == sorted(str(k) for k in [obj1.key, obj2.key])
