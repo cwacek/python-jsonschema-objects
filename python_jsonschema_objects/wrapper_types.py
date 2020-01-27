b516aff7854799d1fe1c693a79dde8eb0daed035
Chris Wacek <cwacek@gmail.com>
2020-01-26 21:07:15 -0500

    Fix 188: Check for ArrayWrapper, then collections.Sequence

--- python_jsonschema_objects/wrapper_types.py
+++ python_jsonschema_objects/wrapper_types.py
@@ -64,10 +64,10 @@ class ArrayWrapper(collections.MutableSequence):
         """ Holds a typed copy of the array """
         self._typed = None
 
-        if isinstance(ary, (list, tuple, collections.Sequence)):
-            self.data = ary
-        elif isinstance(ary, ArrayWrapper):
+        if isinstance(ary, ArrayWrapper):
             self.data = ary.data
+        elif isinstance(ary, (list, tuple, collections.Sequence)):
+            self.data = ary
         else:
             raise TypeError("Invalid value given to array validator: {0}".format(ary))
 
