8efefab9de6ae31321a53185f3d72ca28d803a96
Chris Wacek <cwacek@gmail.com>
2021-05-11 20:58:51 -0400

    Fix #213: Properly support total ordering for literals

---
+++ test/test_regression_213.py
@@ -0,0 +1,36 @@
+import json
+import pytest
+import python_jsonschema_objects as pjo
+
+schema = """
+{
+    "title":"whatever",
+    "properties": {
+        "test": {"type": "number"}
+    }
+}
+"""
+
+
+@pytest.fixture
+def schema_json():
+    return json.loads(schema)
+
+
+def test_literals_support_comparisons(schema_json):
+    builder = pjo.ObjectBuilder(schema_json)
+    ns = builder.build_classes()
+
+    thing1 = ns.Whatever()
+    thing2 = ns.Whatever()
+
+    thing1.test = 10
+    thing2.test = 12
+
+    assert thing1.test < thing2.test
+    assert thing1.test == thing1.test
+
+    thing2.test = 10
+
+    assert thing1.test <= thing2.test
+    assert thing1.test == thing2.test
