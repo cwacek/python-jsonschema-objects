770ee146947055bab57711ae59e8bb49d19062e5
Eliah Kagan <degeneracypressure@gmail.com>
2023-08-14 02:35:41 -0400

    build: Update versioneer from 0.15 to 0.29
    One benefit of this update is to support the upcoming Python 3.12
    release. Old versioneer versions use configparser.SafeConfigParser.
    This was deprecated back in Python 3.2, and being removed in 3.12.
    
    Procedure followed for the update:
    
    - Manually removed the code appended by the old version to the end
      of __init__.py, which the update will not do. (When this is not
      done, it appends improved but similar code after it.)
    
    - In the top-level repository directory (i.e., the directory that
      directly contains setup.py), followed the current instructions
      for a "Vendored mode" installation, by installing the versioneer
      package in a temporary virtual environment and running
      "versioneer install --vendor".
    
    As expected, this reported that the old versioneer.py was replaced,
    and that the effect of running the new script was to create
    _version.py and append to __init__.py.

--- python_jsonschema_objects/__init__.py
+++ python_jsonschema_objects/__init__.py
@@ -161,7 +161,5 @@ class ObjectBuilder(object):
 if __name__ == "__main__":
     validator = ObjectBuilder("../../protocol/json/schema.json")
 
-from ._version import get_versions
-
-__version__ = get_versions()["version"]
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
